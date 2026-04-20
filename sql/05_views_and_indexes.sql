b943a4f326d33d3cfb1df4e2b1ba0f848092ff34
Tessa Smith <jffan4evr@gmail.com>
2026-04-20 16:31:08 -0600

    Add view for high salary employees and optimize index
    Added a view for employees with salaries over 6000 and created an index on first names. Included execution plan checks before and after index creation.

--- sql/05_views_and_indexes.sql
+++ sql/05_views_and_indexes.sql
@@ -1,8 +1,34 @@
 -- Views and Indexes
 -- This file contains view creation and index optimization queries.
 
-CREATE VIEW employee_salary_view AS
-SELECT employee_id, first_name, last_name, salary
-FROM employees;
+-- Create a view for employees in various countries whose salary is more than 6000
+CREATE VIEW countriessalary6000 AS
+SELECT
+    emp_id,
+    first_name,
+    last_name,
+    country,
+    salary
+FROM emp_records
+WHERE salary > 6000
+ORDER BY country;
 
-CREATE INDEX idx_salary ON employees(salary);
+SELECT *
+FROM countriessalary6000;
+
+-- Check execution plan before creating index
+EXPLAIN
+SELECT *
+FROM emp_records
+WHERE first_name = 'Eric';
+
+-- Create an index to improve search performance on FIRST_NAME
+CREATE INDEX idx_first_name
+ON emp_records(first_name);
+
+-- Check execution plan after creating index
+EXPLAIN
+SELECT *
+FROM emp_records
+WHERE first_name = 'Eric';
+
